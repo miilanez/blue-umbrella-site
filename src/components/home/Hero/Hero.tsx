8556b04693fb74fdb0ad8e9451cb65748747c945
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2024-04-18 15:26:07 -0300

    feat: criação e estruturação da pagina

---
+++ src/components/home/Hero/Hero.tsx
@@ -0,0 +1,5 @@
+const Hero = () => {
+  return <div className="bg-slate-100 pt-10 ">Its a Hero</div>;
+};
+
+export default Hero;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,19 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '15px',
+    },
+    screens: {
+      sm: '640px',
+      mg: '768px',
+      lg: '1024px',
+      xl: '1480px',
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        hero: '',
       },
     },
   },
