0bf98c45aa2695266a56fcb9e2813c9982663e71
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2024-04-23 23:20:39 -0300

    :broom: cleanup: eliminando elementos desnecessários para a execução do código

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { ScrollLink } from "react-scroll";
 import ContactButton from "./ContactButton";
 import Image from "next/image";
 import {
